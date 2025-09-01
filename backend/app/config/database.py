a4943c907e2a908112950de3430faa389094868a
Mehul Verma <mehulvermajpr@gmail.com>
2025-09-02 01:45:50 +0530

    updated database url

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -7,15 +7,16 @@ from urllib.parse import quote_plus
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+# DB_HOST = os.getenv("DB_HOST")
+# DB_PORT = os.getenv("DB_PORT")
+# DB_USER = os.getenv("DB_USER")
+# DB_PASSWORD = os.getenv("DB_PASSWORD")
+# DB_NAME = os.getenv("DB_NAME")
 # encoded_password = quote_plus(DB_PASSWORD)
 
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # ✅ Async engine
 engine = create_async_engine(DATABASE_URL, echo=True)
